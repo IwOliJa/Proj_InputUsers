fd3b2478e0cd45977e207ec5f4633db26cf178e2
OlgaKazaka <olgakazaka@yahoo.com>
2023-09-23 18:03:57 +0200

    new commit

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const nameInp = document.querySelector(".name");
 const emailInp = document.querySelector(".email");
 const websiteInp = document.querySelector(".website");
 const body = document.querySelector("body");
-const input = document.querySelector("input");
-
 const nextBtn = document.querySelector(".next");
 const lastBtn = document.querySelector(".last");
+const editBtn = document.querySelector(".edit");
+
 const pName = document.createElement("p");
 const pEmail = document.createElement("p");
 const pWebsite = document.createElement("p");
@@ -16,13 +16,14 @@ const card = document.createElement("div");
 body.append(card);
 card.classList = "card";
 card.append(pName, pEmail, pWebsite, lastBtn, nextBtn);
-
 let entpoint = 1;
 let url = "https://jsonplaceholder.typicode.com/users";
-url = `${url}/${entpoint}`;
+let url2;
+
 fetchData();
 
 function fetchData() {
+  url = `${url}/${entpoint}`;
   fetch(url)
     .then((response) => response.json())
     .then((jsonData) => {
@@ -31,15 +32,16 @@ function fetchData() {
 }
 
 function createCard(jsonData) {
-  // nameInp.value = jsonData.name;
   nameInp.value = "";
   pName.innerText = `Name: ${jsonData.name}`;
-  // emailInp.value = jsonData.email;
   emailInp.value = "";
   pEmail.innerText = `Email: ${jsonData.email}`;
-  // websiteInp.value = jsonData.website;
   websiteInp.value = "";
   pWebsite.innerText = `Website: ${jsonData.website}`;
+  let url = "https://jsonplaceholder.typicode.com/users";
+  url2 = `${url}?id=${jsonData.id}`;
+  // console.log(url2);
+  return url2;
 }
 
 nextBtn.addEventListener("click", () => {
@@ -56,18 +58,18 @@ nextBtn.addEventListener("click", () => {
     })
     .then((objLength) => {
       if (entpoint >= objLength) entpoint = 0;
-
       return (url = `${url}/${++entpoint}`);
     })
     .then((url) => {
       fetch(url)
         .then((response) => response.json())
         .then((jsonData) => {
-          console.log(jsonData);
+          // console.log(jsonData);
           return createCard(jsonData);
         });
     });
 });
+
 lastBtn.addEventListener("click", () => {
   let url = "https://jsonplaceholder.typicode.com/users";
   fetch(url)
@@ -89,127 +91,26 @@ lastBtn.addEventListener("click", () => {
       fetch(url)
         .then((response) => response.json())
         .then((jsonData) => {
-          console.log(jsonData);
+          // console.log(jsonData);
           return createCard(jsonData);
         });
     });
 });
 
-//---------заготовки------------------------
+editBtn.addEventListener("click", () => {
+  const newData = {
+    name: nameInp.value,
+    email: emailInp.value,
+    website: websiteInp.value,
+  };
 
-// });
-// input.addEventListener("click", () => {
-//   nameInp.value = "";
-//   emailInp.value = "";
-//   websiteInp.value = "";
-// });
-
-// .then((jsonData) => {
-//   nameInp.value = jsonData.name;
-//   pName.innerText = `Name: ${jsonData.name}`;
-//   return jsonData;
-// })
-// .then((jsonData) => {
-//   emailInp.value = jsonData.email;
-//   pEmail.innerText = `Email: ${jsonData.email}`;
-//   return jsonData;
-// })
-// .then((jsonData) => {
-//   websiteInp.value = jsonData.website;
-//   pWebsite.innerText = `Website: ${jsonData.website}`;
-//   return jsonData;
-// })
-// .then((jsonData)=>{
-// })
-// }
-
-//----------------------------------------------------------
-// const objLength = Object.keys(jsonData).length;
-// console.log(objLength);
-//----------------------------------------------------------
-
-// let entpoint = 1;
-// let url = "https://jsonplaceholder.typicode.com/users";
-// url = `${url}/${entpoint}`;
-// // fetchData();
-
-// // function fetchData() {
-// fetch(url)
-//   .then((response) => response.json())
-//   .then((jsonData) => {
-//     return jsonData;
-//   })
-//   .then((jsonData) => {
-//     nameInp.value = jsonData.name;
-//     return jsonData;
-//   })
-//   .then((jsonData) => {
-//     emailInp.value = jsonData.email;
-//     return jsonData;
-//   })
-//   .then((jsonData) => {
-//     websiteInp.value = jsonData.website;
-//     //   console.log(jsonData);
-//     //   return jsonData;
-//   })
-//   .then(() => {
-//     nextBtn.addEventListener("click", () => {
-//       let url = "https://jsonplaceholder.typicode.com/users";
-//       url = `${url}/${++entpoint}`;
-
-//       if (entpoint >= 10) {
-//         entpoint = 0;
-//       }
-//       fetch(url)
-//         .then((response) => response.json())
-//         .then((jsonData) => {
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           nameInp.value = jsonData.name;
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           emailInp.value = jsonData.email;
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           websiteInp.value = jsonData.website;
-//           //   console.log(jsonData);
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           console.log(jsonData);
-//         });
-//     });
-//   })
-//   .then(() => {
-//     lastBtn.addEventListener("click", () => {
-//       let url = "https://jsonplaceholder.typicode.com/users";
-//       if (entpoint <= 0) entpoint = 10;
-//       url = `${url}/${entpoint--}`;
-
-//       fetch(url)
-//         .then((response) => response.json())
-//         .then((jsonData) => {
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           nameInp.value = jsonData.name;
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           emailInp.value = jsonData.email;
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           websiteInp.value = jsonData.website;
-//           //   console.log(jsonData);
-//           return jsonData;
-//         })
-//         .then((jsonData) => {
-//           console.log(jsonData);
-//         });
-//     });
-//   });
-// // }
+  fetch(url2, {
+    method: "PUT",
+    body: JSON.stringify(newData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  console.log(newData);
+  createCard(newData);
+});
